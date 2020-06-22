4bbac4d0841aee4465ee558c42d5b3c576ad1e35
carzy <carzy.dong@tonmx.com>
2020-06-22 13:46:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# springboot-study
+
+> 学习springboot与各种模块的使用
+
+## 1. springboot-jdbctemplate
+[springboot-jdbctemplate](boot-jdbctemplate/README.md)
---
+++ boot-jdbctemplate/src/main/java/com/dxp/jdbctemplate/JdbctemplateApplication.java
@@ -0,0 +1,18 @@
+package com.dxp.jdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author carzy
+ */
+@SpringBootApplication
+public class JdbctemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbctemplateApplication.class, args);
+    }
+
+}
