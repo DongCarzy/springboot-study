61478f691784c9cd6182bbd057bb33091fc9dacb
carzy <carzy.dong@tonmx.com>
2020-07-20 18:29:25 +0800

    feat(security): 引入start-web

--- boot-security/README.md
+++ boot-security/README.md
@@ -1,3 +1,5 @@
 # SPRINGBOOT-SECURITY
 
 springboot 项目中采用 `security` 作为系统权限管控.
+
+## security流程
---
+++ boot-security/src/main/java/com/dxp/security/Application.java
@@ -0,0 +1,18 @@
+package com.dxp.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author carzy
+ * @date 2020/7/20
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
